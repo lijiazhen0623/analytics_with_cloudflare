a746cae93c648d00006623c02b30fdc971d38dd8
李家震 <74167577+lijiazhen0623@users.noreply.github.com>
2024-12-08 00:16:39 +0800

    优化js

--- front/dist/index.js
+++ front/dist/index.js
@@ -1,18 +1,4 @@
-(function(){
-
-  console.info(
-    'welcome to WebViso/yestool，author：YesTool，author url: https://webviso.yestool.org'
-  );
-
-  setTimeout(function () {
-    var addHeadStr = '<meta property="og:site_counter_author" content="yestool"></meta>'
-      + '<meta property="og:site_counter_author_url" content="https://webviso.yestool.org"></meta>';
-    
-    if (document.head){
-      document.head.innerHTML += addHeadStr;
-    }
-  }, 500);
-
+(function () {
   const script = document.currentScript;
   let dataBaseUrl = script.getAttribute('data-base-url');
   let dataPagePvId = script.getAttribute('data-page-pv-id');
@@ -22,15 +8,15 @@
   WebViso.version = '0.0.0';
   let BASE_API_PATH = 'https://webviso.yestool.org';
 
-  WebViso.page_pv_id = "page_pv"; 
+  WebViso.page_pv_id = "page_pv";
   WebViso.page_uv_id = "page_uv";
-  if(dataBaseUrl) {
+  if (dataBaseUrl) {
     BASE_API_PATH = dataBaseUrl;
   }
-  if(dataPagePvId) {
+  if (dataPagePvId) {
     WebViso.page_pv_id = dataPagePvId;
   }
-  if(dataPageUvId) {
+  if (dataPageUvId) {
     WebViso.page_uv_id = dataPageUvId;
   }
 
@@ -42,11 +28,18 @@
     const thisPage = getLocation(window.location.href);
     const pagePvEle = document.getElementById(WebViso.page_pv_id);
     const pageUvEle = document.getElementById(WebViso.page_uv_id);
+    const loadingMessage = document.getElementById('loading-message'); // 获取加载提示元素
+
+    // 显示加载提示
+    if (loadingMessage) {
+      loadingMessage.style.display = 'inline';
+    }
+
     const queryData = {
       url: thisPage.pathname,
       hostname: thisPage.hostname,
       referrer: document.referrer
-    }
+    };
     if (pagePvEle) {
       queryData.pv = true;
     }
@@ -55,11 +48,18 @@
     }
     await fetchJson(`${BASE_API_PATH}/api/visit`, queryData)
       .then((res) => {
-        if (res.ret != 'OK') {
+        if (res.ret !== 'OK') {
           console.error('WebViso.init error', res.message);
           return;
         }
         const resData = res.data;
+
+        // 隐藏加载提示
+        if (loadingMessage) {
+          loadingMessage.style.display = 'none';
+        }
+
+        // 只有在元素存在时，才更新内容
         if (pagePvEle) {
           pagePvEle.innerText = resData.pv;
         }
@@ -69,6 +69,10 @@
       })
       .catch((err) => {
         console.log("WebViso.init fetch error", err);
+        // 隐藏加载提示
+        if (loadingMessage) {
+          loadingMessage.style.display = 'none';
+        }
       });
   };
 
@@ -86,26 +90,34 @@
         },
         body: JSON.stringify(data),
       })
-      .then(res => {
-        return res.json();
-      })
-      .then(function(data) {
-        resolve(data);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+        .then(res => res.json())
+        .then(function (data) {
+          resolve(data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
     });
   }
 
-  const getLocation = function(href) {
+  const getLocation = function (href) {
     const l = document.createElement("a");
     l.href = href;
     return l;
   };
-  
+
+  // 等待 DOM 更新后再初始化
   if (typeof window !== 'undefined') {
-    WebViso.init();
+    // 等待 WebViso.page_uv_id 和 WebViso.page_pv_id 元素出现
+    const checkElementsExist = setInterval(() => {
+      const pagePvEle = document.getElementById(WebViso.page_pv_id);
+      const pageUvEle = document.getElementById(WebViso.page_uv_id);
+      if (pagePvEle && pageUvEle) {
+        clearInterval(checkElementsExist); // 停止轮询
+        WebViso.init();
+      }
+    }, 100); // 每 100ms 检查一次
+
     window.WebViso = WebViso;
   }
 })();
